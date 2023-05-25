4cc22b3d0705581266e847ec6ca3490e54cc7846
Karan <karanpraja902@gmail.com>
2023-05-25 15:36:37 +0530

    evennumber

---
+++ evennumber.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n;
+int i=0;
+cout<<"enter a umber"<<endl;
+cin>>n;
+while(i<=n)
+{
+    if(i%2==0)
+    {cout<<i<<endl;}
+    i=i+1;
+}
+}
